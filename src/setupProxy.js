4b2ef19f2a2297094442ce56292990c8741e2c48
Dikshant20011891 <dikshantjoshi20011891@gmail.com>
2024-02-11 07:32:41 +0530

    new Files

---
+++ src/setupProxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  app.use(
+    '/coingecko',
+    createProxyMiddleware({
+      target: 'https://api.coingecko.com/api/v3',
+      changeOrigin: true,
+      pathRewrite: {
+        '/coingecko': '/',
+      },
+    })
+  );
+};
+
